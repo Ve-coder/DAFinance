16d1a218236804beab01cf705018147eefc477fb
VeronicaFSouza <veronicafs35@gmail.com>
2021-04-08 13:29:11 +0100

    Initialised my repo and added m1c1,c2,c3,c4 and c5

---
+++ m1c1.py
@@ -0,0 +1,38 @@
+# Example 1, do not modify!
+print(8 / 2)
+
+# Example 2, do not modify!
+print(2**2)
+# Multiplication, with print output
+print(10 * 2)
+
+# Print the average revenue
+average = total / 3
+print(average)
+# Create revenue_1
+revenue_1 = 229.23
+print(revenue_1)
+# Type of company_1
+print(type(company_1))
+
+# Type of year_1
+print(type(year_1))
+# Compare revenue_1 and revenue_2
+print(revenue_1 > revenue_2)
+
+# Type of revenue_1
+print(type(revenue_1))
+# Update data types
+
+# Update data types
+year_1_str = str(year_1)
+revenue_1_str = str(revenue_1)
+
+# Create a complete sentence combining only the string data types
+sentence = 'The revenue of ' + company_1 + ' in ' + year_1_str  + ' was $' + revenue_1_str + ' billion.'
+
+# Print sentence
+print(sentence)
+
+
+
