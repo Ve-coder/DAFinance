6cadd96b9a11ada45091db0b9031b8591227e2a9
VeronicaFSouza <veronicafs35@gmail.com>
2021-04-27 10:29:28 +0100

    Updating m3c3 and C4. Adding m4c1 to my repo

--- m3c3.py
+++ m3c3.py
@@ -110,7 +110,7 @@ print(temp_by_country_city_vs_year.loc["Egypt":"India"])
 print(temp_by_country_city_vs_year.loc[("Egypt", "Cairo"):("India", "Delhi")])
 
 # Subset in both directions at once
-print(temp_by_country_city_vs_year.loc[("Egypt", "Cairo"):("India", "Delhi"), "2005":"2010")
+print(temp_by_country_city_vs_year.loc[("Egypt", "Cairo"):("India", "Delhi"), "2005":"2010"])
 
 # Get the worldwide mean temp by year
 mean_temp_by_year = temp_by_country_city_vs_year.mean()
